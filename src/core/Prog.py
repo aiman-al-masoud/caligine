6c2b7abffaccad57b2e0f002029e44d01e017c58
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2024-07-23 13:48:40 +0200

    rm useless check

--- src/core/Prog.py
+++ src/core/Prog.py
@@ -3,7 +3,6 @@ from typing import TYPE_CHECKING, List, Optional
 from core.Ast import Ast
 from core.Bool import Bool
 from core.MetaInfo import MetaInfo
-from core.Halt import Halt
 
 if TYPE_CHECKING:
     from core.World import World
@@ -19,13 +18,8 @@ class Prog(Ast):
         res:Ast = Bool(value=False)
 
         for statement in self.statements:
-
             res = statement.execute(world)
 
-            if isinstance(res, Halt):
-                res.execute(world)
-                return res
-        
         return res
 
     def subst(self, dictionary: 'Ast') -> 'Ast':
